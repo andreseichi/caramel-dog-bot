7d482bacb15c885f15cd4639b732f71b615c8363
atashiro <andre.tashiro@voxdatati.com.br>
2022-11-11 15:07:45 -0300

    fix: type of ApplicationCommandModule

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -1,26 +1,6 @@
-import { PermissionString } from "discord.js";
-
-export type ApplicationCommandOptionChoice = {
-  readonly name: string;
-  readonly value: string | number;
-};
-
-export type ApplicationCommandOption = {
-  readonly type: number;
-  readonly name: string;
-  readonly description: string;
-  readonly required?: boolean;
-  readonly choices?: Array<ApplicationCommandOptionChoice>;
-  readonly options?: Array<ApplicationCommandOption>;
-};
+import { PermissionString, SlashCommandBuilder } from "discord.js";
 
 export interface ApplicationCommandModule {
-  readonly name: string;
-  readonly description: string;
-  readonly options?: Array<ApplicationCommandOption>;
-  readonly default_permission?: boolean;
-  readonly permissions: PermissionString = "SEND_MESSAGES";
-  readonly usage: string;
-  readonly devOnly?: boolean;
-  readonly execute: (args?: Array<T>) => any;
+  readonly execute: (args?: any) => any;
+  readonly data: SlashCommandBuilder;
 }
