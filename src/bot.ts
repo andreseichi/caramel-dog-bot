c44b93b292f0cc6d3a673b7f965a59ed89a923d9
andreseichi <andre_seichi@hotmail.com>
2022-11-11 12:28:55 -0300

    chore: command handler in progress

--- src/bot.ts
+++ src/bot.ts
@@ -1,11 +1,26 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import { Client, Events, GatewayIntentBits } from "discord.js";
+import { Client, Collection, Events, GatewayIntentBits } from "discord.js";
+import myClient from "./@types/client";
+
+import { readdirSync } from "fs";
+import { join } from "path";
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds],
-});
+}) as myClient;
+
+client.commands = new Collection();
+
+const commandFiles = readdirSync(join(__dirname, "commands")).filter((file) =>
+  file.endsWith(".ts")
+);
+
+for (const file of commandFiles) {
+  // const command = join(__dirname, "commands", `${file}`);
+  // client.commands.set(command.name, command);
+}
 
 client.once(Events.ClientReady, (c) => {
   console.log(`Ready! Logged in as ${c.user.tag}`);
