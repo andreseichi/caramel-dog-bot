0ceabad0a0f8db89fb9f3fc26a3ad0fd677a0057
atashiro <andre.tashiro@voxdatati.com.br>
2022-11-11 16:14:56 -0300

    feat: register global slash commands

---
+++ src/deploy-global-commands.ts
@@ -0,0 +1,47 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import { REST, Routes } from "discord.js";
+
+import { readdirSync } from "fs";
+import { join } from "path";
+
+const clientId = String(process.env.CLIENT_ID);
+
+const commands = [];
+// Grab all the command files from the commands directory you created earlier
+const commandFiles = readdirSync(join(__dirname, "commands")).filter((file) =>
+  file.endsWith(".ts")
+);
+
+// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  commands.push(command.data.toJSON());
+}
+
+// Construct and prepare an instance of the REST module
+const rest = new REST({ version: "10" }).setToken(
+  String(process.env.DISCORD_TOKEN)
+);
+
+// and deploy your commands!
+(async () => {
+  try {
+    console.log(
+      `Started refreshing ${commands.length} application (/) commands.`
+    );
+
+    // The put method is used to fully refresh all global commands with the current set
+    const data: any = await rest.put(Routes.applicationCommands(clientId), {
+      body: commands,
+    });
+
+    console.log(
+      `Successfully reloaded ${data.length} application (/) commands.`
+    );
+  } catch (error) {
+    // And of course, make sure you catch and log any errors!
+    console.error(error);
+  }
+})();
